7fb17da35ed313862068f6582e7f21a8ad235e86
Cat-Hanbit <dbwndyd518@naver.com>
2023-10-22 15:38:18 +0900

    Priority Queue

---
+++ src/main/java/com/CollectionsStudy/PriorityQueueEx.java
@@ -0,0 +1,25 @@
+package com.CollectionsStudy;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueueEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue pq = new PriorityQueue();
+		
+		pq.offer(3);
+		pq.offer(1);
+		pq.offer(5);
+		pq.offer(2);
+		pq.offer(4);
+		System.out.println(pq);
+		
+		Object obj = null;
+		
+		while ((obj = pq.poll()) != null) 
+			System.out.println(obj);
+	}
+
+}
