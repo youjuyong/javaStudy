da95055dc7b6265a5f0ed40341ca6c9d2d7c74c7
Cat-Hanbit <dbwndyd518@naver.com>
2024-03-12 22:01:46 +0900

    백준 2557 입출력과 사칙연산

---
+++ src/main/java/Baekjoon/Baekjoon2557.java
@@ -0,0 +1,18 @@
+package Baekjoon;
+
+public class Baekjoon2557 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 문제 Hello World!를 출력하시오.
+		 * 
+		 * 입력 없음
+		 * 
+		 * 출력 Hello World!를 출력하시오
+		 */
+		
+		System.out.println("Hello World!");
+	}
+
+}
