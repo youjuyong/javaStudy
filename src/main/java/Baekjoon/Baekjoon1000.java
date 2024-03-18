b09d3376ae71a7132bc6c111764afd7428dc067b
Cat-Hanbit <dbwndyd518@naver.com>
2024-03-18 20:42:59 +0900

    백준 사칙연산 1000번

---
+++ src/main/java/Baekjoon/Baekjoon1000.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon1000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * 문제 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+		 * 
+		 * 입력 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+		 * 
+		 * 출력 첫째 줄에 A+B를 출력한다.
+		 */
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String[] input = sc.nextLine().split(" ");
+		System.out.printf("%d",Integer.parseInt(input[0]) + Integer.parseInt(input[1]));
+	}
+
+}
