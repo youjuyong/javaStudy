f312be3c91118cea67a543765c81028b241cd00c
Cat-Hanbit <dbwndyd518@naver.com>
2024-04-08 22:28:19 +0900

    백준 사칙연산 10172

---
+++ src/main/java/Baekjoon/Baekjoon10172.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+public class Baekjoon10172 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * 아래 예제와 같이 개를 출력하시오.
+		 * 
+		 * 입력 없음.
+		 * 
+		 * 개를 출력한다.
+		 */
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+
+}
