4d7835f7a3bfcd54712af47ce454dcad09976615
Cat-Hanbit <dbwndyd518@naver.com>
2024-04-08 22:17:15 +0900

    백준 사칙연산 10171

---
+++ src/main/java/Baekjoon/Baekjoon10171.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+public class Baekjoon10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 문제 아래 예제와 같이 고양이를 출력하시오.
+		 * 
+		 * 입력 없음.
+		 * 
+		 * 출력 고양이를 출력한다.
+		 */
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+
+}
